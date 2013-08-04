39a1f02943e4bc4eddd641faaf29a7b6ab76446b
Mac <mac@abctech-thailand.com>
2013-08-04 15:51:46 +0700

    Setup Spring webapp

---
+++ src/main/java/com/mac/riakcswebclient/webapp/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.mac.riakcswebclient.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = {"/", "index"})
+    public String index() {
+
+        return "index";
+    }
+}
