f56dea51a5921fc14e25a4265ea1b547cea37af0
Mac <mac@abctech-thailand.com>
2013-08-04 16:41:28 +0700

    Setup Amazon WS liband Riak CS config

--- src/main/java/com/mac/riakcswebclient/webapp/controller/MainController.java
+++ src/main/java/com/mac/riakcswebclient/webapp/controller/MainController.java
@@ -1,14 +1,33 @@
 package com.mac.riakcswebclient.webapp.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    @Autowired
+    private AmazonS3Client amazonS3Client;
+
+    /**
+     * Front page.
+     */
     @RequestMapping(value = {"/", "index"})
     public String index() {
+        return "redirect:/bucket";
+    }
 
-        return "index";
+    /**
+     * View all bucket.
+     */
+    @RequestMapping(value = "bucket")
+    public String bucket() {
+        return "bucket";
     }
 }
